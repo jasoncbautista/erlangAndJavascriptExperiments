24cc4a33ade2c2b2dc4f1f7763b6e05be5d575e8
Jason B <bjason@gmail.com>
2013-12-27 01:46:19 -0800

    Simple binary tree sketch

---
+++ BinaryTree.js
@@ -0,0 +1,65 @@
+
+var BinaryTree = function(){
+    // Constructor
+};
+
+// Node struct:
+var nodeTemplate =  {
+      left: null
+    , right: null
+    , data:  null
+
+};
+
+
+
+
+// Construct a simple binary tree:
+
+
+/**
+ * A simple way to add a new node to a given target node:
+ * @param {type} node,
+ * @param {type} newData,
+ * @param {type} targetBranch,
+ * @return {Null}
+ */
+var addNode( node, newData, targetBranch) {
+    var newNode = {
+            data: newData
+        , right: null
+        , left: null
+    };
+
+    node[targetBranch] = newNode;
+    return node;
+}
+
+
+var Tree = {
+    left: null
+    , right: null
+};
+
+// Left Branch
+var coolChild = addNode(Tree, "cool", "left");
+var anotherChild  addNode(coolChild , "another", "right");
+
+
+// Right
+//
+
+var notCoolChild = addNode(Tree, "not cool", "right");
+var notCoolChild2 = addNode(notCoolChild, "not cool 2", "right");
+var notCoolChild3 = addNode(notCoolChild2, "not cool 3", "left");
+
+
+console.log("tree");
+console.log(Tree);
+// Breadth first search:
+
+var Stack = [];
+
+
+
+
